9b8f80fe9a0444c8428b804fb7a54429b5125508
r0nery <romahasla@gmail.com>
2023-07-09 19:20:51 +0300

    Добавлены методы для самостоятельной игры

--- Monty Hall Paradox Simulation/Simulation.cs
+++ Monty Hall Paradox Simulation/Simulation.cs
@@ -10,7 +10,7 @@ namespace Monty_Hall_Paradox_Simulation
     {
         public static bool GoodDescision(Set set, int choice)
         {
-            int open = 0;
+            int open = -1;
 
             if (set.LoosingNums.Contains(choice))
             {
@@ -40,5 +40,49 @@ namespace Monty_Hall_Paradox_Simulation
             return set.Check(choice);
         }
 
+        public static bool PlayerDescision(Set set, int choice)
+        {
+            int open = 0;
+            //Console.WriteLine("Вы выбрали дверь №" + (choice+1));
+
+            if (set.LoosingNums.Contains(choice))
+            {
+                foreach (int i in set.LoosingNums)
+                {
+                    if (i != choice)
+                        open = i;
+                }
+            }
+            else
+            {
+                open = set.LoosingNums[new Random().Next(2)];
+            }
+
+            int newChoice = -1;
+            for (int i = 0; i < 3; i++)
+            {
+                if (i != choice && i != open)
+                {
+                    newChoice = i;
+                    break;
+                }                    
+            }
+
+            string chaneChoiceMessage =
+                "Вы выбрали дверь №" + (choice+1) +
+                "\nВедущий открывает дверь №" + (open+1) + ", за ней коза. Хотите поменять решение на "+(newChoice+1)+"?\ny/n";
+            ConsoleKey key = KeyInputWaiter.WaitForKey(new List<ConsoleKey>() { ConsoleKey.Y, ConsoleKey.N }, chaneChoiceMessage);
+
+            switch (key)
+            {
+                case ConsoleKey.Y:
+                    return set.Check(newChoice);   
+                case ConsoleKey.N:
+                    return set.Check(choice);
+            }
+
+            return false;
+
+        }
     }
 }
