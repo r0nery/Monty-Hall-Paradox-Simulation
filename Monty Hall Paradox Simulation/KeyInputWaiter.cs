32a0ed21523adab7eb1ee40d654716c9ebafab69
r0nery <romahasla@gmail.com>
2023-07-09 19:09:14 +0300

    Добавлен класс KeyInputWaiter, содержащий метод WaitForKey, который ожидает ввода в консоль клавиши, принадлежащей списку, и возвращает эту клавишу

---
+++ Monty Hall Paradox Simulation/KeyInputWaiter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monty_Hall_Paradox_Simulation
+{
+    internal class KeyInputWaiter
+    {
+        public static ConsoleKey WaitForKey (List<ConsoleKey> keys, string message)
+        {
+            ConsoleKey key;
+            do
+            {
+                Console.WriteLine(message);
+                key = Console.ReadKey().Key;
+                Console.Clear();
+            }
+            while (!keys.Contains(key));
+            return key;
+        }
+    }
+}
